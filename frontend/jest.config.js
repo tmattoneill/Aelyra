c76a29143d3fe82c4e067561ba3c29eb72b9c192
Matt O'Neill <tmattoneill@gmail.com>
2025-06-26 11:10:08 +0100

    updating vite

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
